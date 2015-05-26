5a8b60b21018e8966ee47b37f17b1f3061a73677
James Woodley <info@n17solutions.com>
2015-05-26 12:49:18 +0100

    v0.0.4 Standalone Browserifying

--- gulpfile.js
+++ gulpfile.js
@@ -19,8 +19,10 @@ gulp.task('cleanOutput', function() {
 });
 
 gulp.task('browserify', function() {
-	return browserify('./js/app.js', { 
-			debug: true
+	return browserify('./js/app.js', {
+			// list of modules to make require-able externally
+			require: ['jquery', 'angular'],
+			standalone: 'n17-tooltip'
 		})
 		.bundle()
 		.pipe(source('app.js'))
